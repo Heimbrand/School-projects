80c300fca32be44d15587fd70d77aaff1a086883
Heimbrand <Olle.Heimbrand@hotmail.se>
2023-09-01 15:31:34 +0200

    övningsuppgifter

--- Övningsuppgifter/Övningsuppgifter/Program.cs
+++ Övningsuppgifter/Övningsuppgifter/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿// Uppgift 1
+
 //skriv ett program som frågar efter användarens namn och skriver ut en hälsning på konsolen.
 //om namnet är "david" ska det skriva ut "hej david!"
 
@@ -7,6 +8,8 @@
 //Console.WriteLine("Hej, " + name + "!");
 
 //---------------------------------------------------------------------------------
+
+//Uppgift 2
 
 //Skriv ett program som frågar användaren efter ett lösenord. 
 //Hitta på ett hemligt lösenord och spara det i en variabel. 
@@ -27,6 +30,8 @@
 //}
 
 //----------------------------------------------------------------------------------
+
+//Uppgift 3
 
 //Skriv ett program som frågar användaren efter ett tal. 
 //Det ska skriva ut om talet är mindre än 100, lika med 100 eller större
@@ -60,6 +65,8 @@
 
 //-------------------------------------------------------------------------------------------
 
+//Uppgift 4
+
 //Uppdatera programmet i uppgift 1 så att det även frågar efter ett tal. 
 //Skriv hälsningen så många gånger som användaren anger.
 
@@ -79,15 +86,95 @@
 
 //--------------------------------------------------------------------------------------------
 
+//Uppgift 5
 
 //Skriv ett program som upprepade gånger frågar användaren efter ett tal ända till man skriver något som inte är ett tal (t.ex bara trycker enter). 
 //Efter varje inmatning ska summan av alla tal som matats in skrivas ut, innan nästa inmatning efterfrågas. 
 //Innan programmet avslutas ska man även skriva ut medelvärde av de inmatade talen. Hint: TryParse()
 
-string inputtedvalue = "";
-int num;
-int sum = 0;
-int.TryParse(inputtedvalue, out num);
-Console.WriteLine("Skriv in ett tal");
-Console.ReadLine();
+using System.Reflection.Metadata.Ecma335;  //Vad är detta? :O
+using System.Xml;
 
+//string inputtedvalue = "";
+//int num;
+//int sum = 0;
+//int iterations = 0;
+//int.TryParse(inputtedvalue, out num);
+//bool correctinput = true;
+//while (correctinput)
+//{
+//    Console.WriteLine("Skriv in ett tal");
+//    inputtedvalue = Console.ReadLine();
+
+
+
+//    if (int.TryParse(inputtedvalue, out num))
+//    {
+//        Console.WriteLine("Du skrev in talet: " + inputtedvalue);
+//        sum = sum + num;
+//        Console.WriteLine("Summan av dina tal är: " + sum);
+//        iterations++;
+
+//    }
+//    else
+//    {
+//        Console.WriteLine("Du skrev inte in ett tal");
+//        int avg = sum / iterations; 
+//        Console.WriteLine("Medelvärdet av dina inskrivna tal är: " + avg);
+//        correctinput = false;
+//    }
+//}
+
+//---------------------------------------------------------------------------------------------------------
+
+//Uppgift 6
+
+//Skriv ett program som först frågar efter ett tal, sedan frågar efter ett av följande tecken: +, -, *eller /
+//Därefter ska ytterligare ett tal efterfrågas. Programmet ska fungera som en simpel miniräknare. 
+//Om man t.ex matat in först 3, sedan *, och sist 5, så ska programmet skriva ut: 3 * 5 = 15.
+
+double firstInput;
+double secondInput;
+double inputSum;
+string calculator;
+
+try
+{
+
+    Console.WriteLine("Hej och välkomna till abrahams magiska matteäventyr");
+    Console.WriteLine("Skriv ett tal");
+    firstInput = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Kanon, nu skall du skriva in en operand!");
+    calculator = Console.ReadLine();
+    Console.WriteLine("Mycket bra! skriv nu ett till tal");
+    secondInput = Convert.ToDouble(Console.ReadLine());
+
+    if (calculator == "+")
+    {
+        inputSum = firstInput + secondInput;
+        Console.WriteLine("additionen av dina tal är: " + inputSum);
+    }
+    else if (calculator == "-")
+    {
+        inputSum = firstInput - secondInput;
+        Console.WriteLine("Subtrationen av dina tal är: " + inputSum);
+    }
+    else if (calculator == "/")
+    {
+        inputSum = firstInput / secondInput;
+        Console.WriteLine("Divisionen av dina tal är: " + inputSum);
+    }
+    else if (calculator == "*")
+    {
+        inputSum = firstInput * secondInput;
+        Console.WriteLine("Multiplikationen av dina tal är: " + inputSum);
+    }
+    else
+    {
+        Console.WriteLine("Du e fan dum i huvudet");
+    }
+}
+catch 
+{
+    Console.WriteLine("Du skrev inte in ett tal");
+}
