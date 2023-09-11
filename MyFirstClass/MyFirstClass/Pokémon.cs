9c00d6fa336120ed86eff2e2481e9c9c03a7c969
Heimbrand <Olle.Heimbrand@hotmail.se>
2023-09-11 12:29:38 +0200

    MyFirstClass

---
+++ MyFirstClass/MyFirstClass/Pokémon.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace MyFirstClass
+{
+    internal class Pokémon
+    {
+        public string _name;
+        public string _type;
+        public int _level;
+        public string _catchphrase;
+    }
+}
---
+++ MyFirstClass/MyFirstClass/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+
+
+using MyFirstClass;
+using System;
+
+Pokémon pikachu = new Pokémon();
+pikachu._name = "pikachu";
+pikachu._type = "electric";
+pikachu. _level = 66;
+pikachu._catchphrase = "PIKA PIKA PIKAAAA!!1!!111";
+
+Pokémon Blastoise = new Pokémon();
+Blastoise._name = "Blastoise";
+Blastoise._type = "Water";
+Blastoise._level = 99;
+Blastoise._catchphrase = "Hello, how are you. I am under water";
+
+Console.WriteLine(Blastoise._name);
+Console.WriteLine(Blastoise._type);
+Console.WriteLine(Blastoise._catchphrase);
+Console.WriteLine(Blastoise._level);
+Console.WriteLine("--------------------");
+Console.WriteLine(pikachu._name);
+Console.WriteLine(pikachu._type);
+Console.WriteLine(pikachu._catchphrase);
+Console.WriteLine(pikachu._level);
