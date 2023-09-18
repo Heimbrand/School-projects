524a02d1b6ee50f880bfec1ab76777ea7861e8cf
Heimbrand <Olle.Heimbrand@hotmail.se>
2023-09-18 11:27:04 +0200

    updated

---
+++ MyFirstClassDiagram/GameObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstClassDiagram
+{
+    internal class GameObject
+    {
+        public bool isDead { get; set; }
+        public Vector2 Position { get; set; }
+        public float Radius { get; set; }
+        public Vector2 Speed { get; set; }
+        public float Rotation { get; set; }
+
+        public bool CollidesWith(GameObject other)
+        {
+            return true;
+        }
+        
+       
+
+    }
+}
