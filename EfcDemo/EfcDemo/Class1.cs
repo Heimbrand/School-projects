402a4a182009d31c66cb81b5ffc4bfa2f0ae6641
Heimbrand <Olle.Heimbrand@hotmail.se>
2023-11-30 14:26:55 +0100

    Setup new db via vs

---
+++ EfcDemo/EfcDemo/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace EfcDemo;
+
+//"Data Source=OLLES;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"
+
+
+
+  
+
---
+++ MinFörstaEntityFramework/MinFörstaEntityFramework/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Mime;
+
+Console.WriteLine("Hello, World!");
+
+
+//"Data Source=OLLES;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"
+
