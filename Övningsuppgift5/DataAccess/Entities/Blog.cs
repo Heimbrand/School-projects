c043ea4a8b22b2da84f8813f40b7a713f2a7fe18
Heimbrand <Olle.Heimbrand@hotmail.se>
2023-12-05 12:35:25 +0100

    Fixed problem with relations between comments and blogs

--- Övningsuppgift5/DataAccess/Entities/Blog.cs
+++ Övningsuppgift5/DataAccess/Entities/Blog.cs
@@ -9,6 +9,6 @@ public class Blog
     public int ID { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
-    public List<Comment> Comments { get; set; }
+    public List<Comment> Comments { get; set; } = new();
 
 }
