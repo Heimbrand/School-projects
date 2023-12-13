772482493cec6a46273c9b9a32f80a01244fb81b
Heimbrand <Olle.Heimbrand@hotmail.se>
2023-12-13 12:58:30 +0100

    Delete Class1.cs

--- EfcDemo/EfcDemo/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EfcDemo;
-
-//"Data Source=OLLES;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"
-
-
-
-  
-
